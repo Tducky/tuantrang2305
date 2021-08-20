452ee656b41778cf33be88d1108110be1c9972e8
“DINHMINHTUAN” <dinhminhtuan@MacBook-Pro-cua-INH.local>
2021-08-20 10:46:55 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Em yêu à!!! Hôm nay 23/8/2021 là kỉ niệm 3 tháng mình yêu nhau nè💓💓',
+    introDesc: `Ba ngàn con sông chỉ một gáo nước, 
+    thiên hạ 3 vạn người anh chỉ muốn cưới em`,
+    btnIntro: 'Muốn xem trò của anh yêu tiếp thì bấm vào đây nhé ',
+    title: 'Phải chăng mình sinh ra là để dành cho nhau em yêu nhỉ 😘😘',
+    desc: 'Em có đồng ý cưới anh, muốn làm mẹ của các con anh không 😝😝',
+    btnYes: 'Em đồng ý ❤️❤️',
+    btnNo: 'Còn phải xem xét thái độ 🙄🙄',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại muốn cưới anh',
+    btnReply: 'Gửi cho anh',
+    reply: 'Vì em muốn làm mẹ các con của anh',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều lắm lắm lắm luôn ý 😘😘',
+    messDesc: 'Anh mong dịch mau chóng qua đi để mình làm đám cưới nhé vợ yêu ❤️❤️',
+    btnAccept: 'Ok chồng yêu 😘😘 ',
+    messLink: 'https://www.youtube.com/watch?v=jZQ4F1XFf-s' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
